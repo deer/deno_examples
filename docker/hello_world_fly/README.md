a1281e51dd0d30c7181294a573e57ffe93d86a06
Reed von Redwitz <reed+github@vonredwitz.com>
2023-02-16 10:59:12 +0100

    modifying server response to log host

--- docker/hello_world_fly/README.md
+++ docker/hello_world_fly/README.md
@@ -1,3 +1,8 @@
 This is like the [other](https://github.com/deer/deno_examples/tree/main/docker/hello_world) hello world example (I really just copy and pasted the code), but it's intended to be run on [fly](https://fly.io/).
 
 The `docker-compose.yaml` file is ignored by fly.
+
+to test this locally:
+```
+docker-compose -f src/docker-compose.yaml up --build
+```
--- docker/hello_world_fly/src/server.ts
+++ docker/hello_world_fly/src/server.ts
@@ -1,5 +1,8 @@
 import { serve } from "https://deno.land/std@0.173.0/http/server.ts";
 
-serve(() => new Response("Hello world from Deno! 🦕"), { port: 8080 });
+serve((req: Request) => {
+  console.log(req.headers.get("host"));
+  return new Response("Hello world from Deno! 🦕");
+}, { port: 8080 });
 
 console.log(`HTTP server is running at: http://localhost:8080/`);
